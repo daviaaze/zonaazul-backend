34406860c404cf1e15b48a326d066f13ccf1c1fc
Davi Azevedo <daviaaze@gmail.com>
2020-08-29 08:58:26 -0300

    fixed error in db config

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 })
 module.exports = {
-  host: '127.0.0.1',
+  host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
